4068b93edeb41b2bd966312c35944fe8e155987d
cryptoMavrik <mav@localhost>
2022-06-16 10:16:45 -0400

    add normalization

---
+++ src/styles.tsx
@@ -0,0 +1,131 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', cursive, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    
+  }
+
+  body {
+    /* background: hsl(0 0% 5% / .1); */
+    color: white !important;
+    background: radial-gradient(ellipse at 33% -25%, #0e3489 25%, #880011 83.6%);
+  }
+
+  :root {
+    font-size: 16px;
+
+    @media screen and (max-width: 425px) {
+      font-size: 12px;
+    }
+    /* background: radial-gradient(ellipse at 33% -25%, #0e3489 25%, #880011 83.6%); */
+  }
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center, input,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend, button,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+  }
+
+  button a {
+    text-decoration: none;
+  }
+  
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  
+  ol,
+  ul {
+    list-style: disc;
+    list-style-position: inside;
+  }
+
+  blockquote,
+  q {
+    quotes: none;
+  }
+
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: "";
+    content: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  [role="button"] {
+    cursor: pointer;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  input[type=range] {
+    -webkit-appearance: none; /* Hides the slider so that custom slider can be made */
+    width: 100%; /* Specific width is required for Firefox. */
+    background: transparent; /* Otherwise white in Chrome */
+  }
+
+  input[type=range]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+  }
+
+  input[type=range]:focus {
+    outline: none; /* Removes the blue border. You should probably do some kind of focus styling for accessibility reasons though. */
+  }
+
+  input[type=range]::-ms-track {
+    width: 100%;
+    cursor: pointer;
+    /* Hides the slider so custom styles can be added */
+    background: transparent; 
+    border-color: transparent;
+    color: transparent;
+  }  
+
+  input,
+  button {
+    border: none;
+    outline: none;
+  } 
+`;
+
+export default GlobalStyle
