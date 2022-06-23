b4877f8017f2f6746dce7cfa3ece3ee9f84eb19d
cryptoMavrik <mav@localhost>
2022-06-23 18:35:46 -0400

    patch routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NavMenu from './components/Navbar';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <NavMenu>
       <Routes>
-        <Route path='/' element={<>Hello World</>} />
-        <Route path='/about' element={<>about</>} />
+        <Route path='/' element={<Profile />} />
+        <Route path='/profile' element={<Profile />} />
         <Route path='/portfolio' element={<>portfolio</>} />
         <Route path='/contact' element={<>contact</>} />
       </Routes>
