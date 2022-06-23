e835a2ab42d16d3c73170feae327e578b0ca43c2
cryptoMavrik <mav@localhost>
2022-06-23 10:14:27 -0400

    add nav types

--- src/components/Navbar/types.ts
+++ src/components/Navbar/types.ts
@@ -1,4 +1,16 @@
+import React from "react";
+
+export interface NavMenuProps {
+    activeItem?: string;
+    children?: React.ReactNode | React.ReactNode[]
+}
+
 export type NavItemsType = {
     label: string;
     href: string;
+}
+
+export interface NavItemsProps {
+    items: NavItemsType[];
+    activeItem?: string;
 }
