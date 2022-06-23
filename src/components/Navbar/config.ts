a118ffa36e95f21e4c6aa69f4f6fee1f17afd65f
cryptoMavrik <mav@localhost>
2022-06-23 08:57:22 -0400

    add navigation config

---
+++ src/components/Navbar/config.ts
@@ -0,0 +1,17 @@
+import { NavItemsType } from "./types";
+
+export const links: NavItemsType[] = [
+    {
+        label: "About",
+        href: "/about"
+    },
+    {
+        label: "Portfolio",
+        href: "/portfolio"
+    },
+    {
+        label: "Contact",
+        href: "/contact"
+    },
+
+]
