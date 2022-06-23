335f36cfb37a283d325be0e1196e2a9aa12cc96b
cryptoMavrik <mav@localhost>
2022-06-23 10:14:50 -0400

    create navmenu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { useLocation } from 'react-router-dom'
+import { FlexColumn, FlexRow } from '../../styles'
+import { links } from './config'
+import { NavItem, StyledNavBar, Wrapper } from './styles'
+import { NavMenuProps } from './types'
 
-const NavBar: React.FC = () => {
+const NavMenu: React.FC<NavMenuProps> = ({
+    children
+}) => {
+    const pathname = useLocation()
+
     return (
-        <></>
+        <Wrapper>
+            <StyledNavBar>
+                <div></div>
+                <FlexRow>
+                    {
+                        links.map(({ label, href }, index) => {
+                            return (
+                                <NavItem key={index} $isActive={pathname.pathname === href} to={href}>
+                                    {label}
+                                </NavItem>
+                            )
+                        })
+                    }
+                </FlexRow>
+            </StyledNavBar>
+            <FlexColumn height='100%'>
+                <FlexRow>
+                    {children}
+                </FlexRow>
+            </FlexColumn>
+        </Wrapper>
     )
 }
 
-export default NavBar
+export default NavMenu
