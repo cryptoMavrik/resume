d70fe6811bd6aa357403a3bbb3eabb2baf89da16
cryptoMavrik <mav@localhost>
2022-06-23 10:13:35 -0400

    init react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NavMenu from './components/Navbar';
 
 function App() {
   return (
-    <>
-      Hello World
-    </>
+    <NavMenu>
+      <Routes>
+        <Route path='/' element={<>Hello World</>} />
+        <Route path='/about' element={<>about</>} />
+        <Route path='/portfolio' element={<>portfolio</>} />
+        <Route path='/contact' element={<>contact</>} />
+      </Routes>
+    </NavMenu>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './styles';
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 reportWebVitals();
