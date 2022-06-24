1f919f00078c6f4f816a8ed56e0a7e81238c0ec2
cryptoMavrik <mav@localhost>
2022-06-24 00:19:46 -0400

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ export const Bubble3 = styled.div<{ top?: string, left?: string }>`
   background: radial-gradient(circle, hsl(245 80% 65% / .4), hsl(240 50% 55% / .01) 70%, transparent 100%);
   /* background: radial-gradient(circle, hsl(220 50% 50% / .15), hsl(220 40% 50% / .01) 50%, #050710); */
   height: 60rem;
-  width: 80rem;
+  width: 60rem;
   top: ${({ top }) => top ? top : "0"};
   left: ${({ left }) => left ? left : "0"};
 `;
