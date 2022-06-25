8859b24749a13dd5af25593b6bc49dc4e373046a
cryptoMavrik <mav@localhost>
2022-06-25 09:25:44 -0400

    add contact form component

---
+++ src/pages/Contact/index.tsx
@@ -0,0 +1,90 @@
+import emailjs from "@emailjs/browser"
+import { useEffect, useState } from "react"
+import { FlexColumn, FlexRow, Heading, Text } from "../../styles"
+import { Container, EmailForm, Input, SubmitButton, TextArea } from "./styles"
+
+emailjs.init("NNsqFjJRMcWt4TsU7")
+
+const Contact: React.FC = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    const [isSuccess, setIsSuccess] = useState(false)
+    const [isFail, setIsFail] = useState(false)
+    const handleSuccess = () => {
+        setIsSuccess(true)
+        setTimeout(() => {
+            setIsSuccess(false)
+        }, 3000)
+    }
+    const handleFail = () => {
+        setIsFail(true)
+        setTimeout(() => {
+            setIsFail(false)
+        }, 3000)
+    }
+
+
+    const handleSubmit = () => {
+        setIsSuccess(false)
+        emailjs.sendForm("service_6dt2fbh", "template_6ezaths", "#email_form")
+            .then((res) => {
+                handleSuccess()
+                console.log('SUCCESS!', res.status, res.text);
+            })
+            .catch((error) => {
+                handleFail()
+                console.log('FAILED...', error);
+            });
+    }
+
+    return (
+        <Container>
+            <EmailForm>
+                <FlexRow py={"1rem"}>
+                    <Heading>
+                        Contact Me
+                    </Heading>
+                </FlexRow>
+                <form id="email_form">
+                    <FlexColumn>
+                        <Input
+                            onChange={(e) => setName(e.target.value)}
+                            type="text"
+                            value={name}
+                            required
+                            placeholder="Name"
+                            name="from_name"
+                        />
+                        <Input
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="text"
+                            value={email}
+                            required
+                            placeholder="Email"
+                            name="reply_to"
+                        />
+                        <TextArea
+                            onChange={(e) => setMessage(e.target.value)}
+                            value={message}
+                            required
+                            placeholder="Message"
+                            name="message_html"
+                        />
+                        <SubmitButton type="button" value="Submit" onClick={handleSubmit} />
+                    </FlexColumn>
+                </form>
+            </EmailForm>
+            <FlexRow>
+                <Text scale="sm">
+                    {isSuccess && "Success"}
+                </Text>
+                <Text scale="sm">
+                    {isFail && "Failed to send"}
+                </Text>
+            </FlexRow>
+        </Container>
+    )
+}
+
+export default Contact
---
+++ src/pages/Contact/styles.ts
@@ -0,0 +1,50 @@
+import { motion } from "framer-motion";
+import styled from "styled-components/macro";
+import { MENU_HEIGHT } from "../../components/Navbar/config";
+import { FlexColumn } from "../../styles";
+
+export const Container = styled(motion.div)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: calc(100vh + ${MENU_HEIGHT}); 
+    margin-top: -5rem;
+    padding: 0 9%;
+`
+
+export const EmailForm = styled(FlexColumn)`
+    background: hsl(0 0% 0% / .45);
+    border: 2px solid hsl(180 50% 75% / 1);
+    padding: 2rem 4rem;
+    border-radius: 2rem;
+    width: 100%;
+`
+
+export const Input = styled(motion.input)`
+    background: hsl(0 0% 0% / .1);
+    color: #f3f3f3;
+    border: 2px solid #a2a2a2;
+    border-radius: 8px;
+    width: 325px;
+    padding: .5rem 1rem;
+    margin: 1rem 0;
+`
+export const TextArea = styled(motion.textarea)`
+    background: hsl(0 0% 0% / .25);
+    color: #f3f3f3;
+    border: 2px solid #a2a2a2;
+    border-radius: 8px;
+    width: 100%;
+    padding: 1rem;
+    margin: 1rem 0;
+`
+
+export const SubmitButton = styled(motion.input)`
+    background: hsl(165 50% 50% / .75);
+    color: #f3f3f3;
+    border-radius: 8px;
+    width: 300px;
+    padding: 1rem;
+    margin: 1rem 0;
+`
