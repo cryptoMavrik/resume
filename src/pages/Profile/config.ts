91b8e2993be05e9396f5ec6237a2ba5eecd99370
cryptoMavrik <xdaegonx@gmail.com>
2022-07-02 22:57:06 -0400

    edit skills

--- src/pages/Profile/config.ts
+++ src/pages/Profile/config.ts
@@ -7,20 +7,20 @@ export const skills1 = [
     "React",
     "NextJS",
     "Solidity",
+    "Figma",
     "Python",
     "GraphQL",
     "ERC20 Swap interface",
-    "ERC721 Collections",
 ]
 
 export const skills2 = [
+    "ERC721 Collections",
     "ERC721 Marketplace",
     "Trading View Integration",
     "ERC1155 Staking",
     "ERC20 Launchpad",
     "Token stream interface",
     "Token Pre-sale interface",
-    "Arbitrage Bots",
     "Chain-agnostic Protocols",
     "Swap Aggregation",
     "Wallet integration",
